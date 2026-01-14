492fd0797a9151de170d91946eb89a011c062975
Sarg3n7 <shashwatshivam04@gmail.com>
2026-01-14 19:46:28 +0530

    setup project; implemented todo context wrapper

---
+++ src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo Message",
+            checked: false
+        }
+    ],
+
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleChecker: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+} 
+
+export const TodoProvider = TodoContext.Provider
